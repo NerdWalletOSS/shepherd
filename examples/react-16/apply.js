9bba1365305d4485adfd4fdd381c5ae74cc4f030
Francis John <fjohn@nerdwallet.com>
2018-07-23 10:36:40 -0700

    Update apply.js

--- examples/react-16/apply.js
+++ examples/react-16/apply.js
@@ -10,7 +10,7 @@ if (fs.existsSync(process.cwd() + '/Jenkinsfile') && !fs.existsSync(process.cwd(
   isLibrary = true;
 }
 
-const cmd = `npx @nerdwallet/nw-react react@16 --interactive=false --install=false ${
+const cmd = `npx @nerdwallet/nw-react react@16 --interactive=false ${
   isLibrary ? '--library' : ''
 }`;
 cp.execSync(cmd, { stdio: 'inherit' });
