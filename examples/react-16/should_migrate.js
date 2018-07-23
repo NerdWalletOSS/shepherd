0aeb0c2f2fcdd46efd820416250ff14036241132
Francis John <fjohn@nerdwallet.com>
2018-07-23 10:37:43 -0700

    Exit with code 1 rather than throwing an error

--- examples/react-16/should_migrate.js
+++ examples/react-16/should_migrate.js
@@ -8,11 +8,13 @@ dependencyKeys.forEach(key => {
     hasReact = true;
     // v16 is less than the given range then this app is v16 compatible
     if (!semver.gtr('16.0.0', pkg[key].react)) {
-      throw new Error('This repo already has React 16 compatibility');
+      console.error('This repo already has React 16 compatibility');
+      process.exit(1);
     }
   }
 });
 
 if (!hasReact) {
-  throw new Error('This repo does not use React');
+  console.error('This repo does not use React');
+  process.exit(1);
 }
