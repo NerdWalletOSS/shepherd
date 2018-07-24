131f8b45f60ff0134980d3bb42a5cb8121666020
Nathan Walters <nwalters@nerdwallet.com>
2018-07-24 14:36:43 -0700

    Fix PR message footer

--- src/util/generate-pr-message.ts
+++ src/util/generate-pr-message.ts
@@ -4,6 +4,6 @@ export default (results: IStepsResults): string => {
   let message = results.stepResults.map((r) => r.stdout).filter((r) => r).join('').trim();
   // We'll add a friendly footer too
   message += '\n\n---\n\n';
-  message += '*This change was executed automatically with [Shepherd](https://github.com/NerdWallet/shepherd).* 💚🤖';
+  message += '*This change was executed automatically with [Shepherd](https://github.com/NerdWallet/shepherd2).* 💚🤖';
   return message;
 };
