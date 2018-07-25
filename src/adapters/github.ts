d38dc8ee07478f2a47b7eaef45923ee408f4b3d3
Nathan Walters <nwalters@nerdwallet.com>
2018-07-25 16:57:34 -0700

    Extract abstract class GitAdapter from GithubAdapter

--- src/adapters/github.ts
+++ src/adapters/github.ts
@@ -9,12 +9,13 @@ import simpleGit, { SimpleGit } from 'simple-git/promise';
 import { IMigrationContext } from '../migration-context';
 import { paginateSearch } from '../util/octokit';
 import IRepoAdapter, { IRepo } from './base';
+import GitAdapter from './git';
 
-class GithubAdapter implements IRepoAdapter {
-  private migrationContext: IMigrationContext;
+class GithubAdapter extends GitAdapter {
   private octokit: Octokit;
-  private branchName: string;
+
   constructor(migrationContext: IMigrationContext) {
+    super(migrationContext);
     this.migrationContext = migrationContext;
     this.branchName = migrationContext.migration.spec.id;
 
@@ -60,42 +61,6 @@ class GithubAdapter implements IRepoAdapter {
     return `${owner}/${name}`;
   }
 
-  public async checkoutRepo(repo: IRepo): Promise<void> {
-    const repoPath = `git@github.com:${repo.owner}/${repo.name}.git`;
-    const localPath = this.getRepoDir(repo);
-
-    if (await fs.existsAsync(localPath) && await this.git(repo).checkIsRepo()) {
-      // Repo already exists; just fetch
-      await this.git(repo).fetch('origin');
-    } else {
-      await simpleGit().clone(repoPath, localPath);
-    }
-
-    // We'll immediately create and switch to a new branch
-    try {
-      await this.git(repo).checkoutLocalBranch(this.branchName);
-    } catch (e) {
-      // This branch probably already exists; we'll just switch to it
-      // to make sure we're on the right branch for the commit phase
-      await this.git(repo).checkout(this.branchName);
-    }
-  }
-
-  public async commitRepo(repo: IRepo): Promise<void> {
-    const { migration: { spec } } = this.migrationContext;
-    await this.git(repo).add('.');
-    await this.git(repo).commit(`Shepherd: ${spec.title}`);
-  }
-
-  public async resetRepo(repo: IRepo): Promise<void> {
-    await this.git(repo).reset('hard');
-    await this.git(repo).clean('f', ['-d']);
-  }
-
-  public async pushRepo(repo: IRepo): Promise<void> {
-    await this.git(repo).push('origin', 'HEAD', { '-f': null });
-  }
-
   public async prRepo(repo: IRepo, message: string): Promise<void> {
     const { migration: { spec } } = this.migrationContext;
     const { owner, name } = repo;
@@ -122,10 +87,8 @@ class GithubAdapter implements IRepoAdapter {
     return path.join(this.migrationContext.migration.workingDirectory, 'data', repo.owner, repo.name);
   }
 
-  private git(repo: IRepo): SimpleGit {
-    const git = simpleGit(this.getRepoDir(repo));
-    git.silent(true);
-    return git;
+  protected getRepositoryUrl(repo: IRepo): string {
+    return `git@github.com:${repo.owner}/${repo.name}.git`;
   }
 }
 
