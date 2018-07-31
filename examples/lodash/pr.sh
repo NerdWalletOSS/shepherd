c4832518e7692615b3812623723d87540c829e64
Nathan Walters <nwalters@nerdwallet.com>
2018-07-31 14:50:28 -0700

    Add basic PR message

---
+++ examples/lodash/pr.sh
@@ -0,0 +1,7 @@
+cat <<-\EOF
+Hey!
+
+This PR updates you to the latest version of Lodash. If you were on
+Lodash 3, your code has been updated with
+[lodash-codemods](https://github.com/jfmengels/lodash-codemods).
+EOF
