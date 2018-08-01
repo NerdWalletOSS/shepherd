9310f3a51a7fe4cfb4f3b0642720924fa75a9a19
Parsha Pourkhomami <parshap+git@gmail.com>
2018-07-31 22:59:45 -0700

    Look at all .jsx files

--- examples/react-16/update_transition_group.sh
+++ examples/react-16/update_transition_group.sh
@@ -8,5 +8,7 @@ set -e
 npm uninstall react-addons-css-transition-group
 npm install react-transition-group@1
 
-git grep -l react-addons-css-transition-group -- client/**/*.js{,x} | \
+i
+git grep -l react-addons-css-transition-group | \
+  grep -e '\.jsx\?$' | \
   xargs sed -i '' 's/react-addons-css-transition-group/@nerdwallet/react-transition-group/g'
