f81c5288c4166f4d173affed4d2d5eeb2c55c75e
Parsha Pourkhomami <parshap+git@gmail.com>
2018-07-31 23:05:47 -0700

    Fix react-transition-group script

--- examples/react-16/update_transition_group.sh
+++ examples/react-16/update_transition_group.sh
@@ -8,7 +8,6 @@ set -e
 npm uninstall react-addons-css-transition-group
 npm install react-transition-group@1
 
-i
 git grep -l react-addons-css-transition-group | \
   grep -e '\.jsx\?$' | \
-  xargs sed -i '' 's/react-addons-css-transition-group/@nerdwallet/react-transition-group/g'
+  xargs sed -i '' 's/react-addons-css-transition-group/react-transition-group/g'
