5a20846ee5aebe62c19d7da92f67072da316f0db
Nathan Walters <nwalters512@gmail.com>
2018-08-02 15:07:08 -0700

    Don't warn about missing directories during checkout (#22)
    * Don't warn about missing directory during checkout
    
    * Fix eslint migration PR message
    
    * Revert change to shepherd.yml
    
    * Address concerns from code review
    
    * Use default value in options destructuring

--- src/util/for-each-repo.ts
+++ src/util/for-each-repo.ts
@@ -5,7 +5,11 @@ import { IMigrationContext } from '../migration-context';
 
 type RepoHandler = (repo: IRepo) => Promise<void>;
 
-export default async (context: IMigrationContext, handler: RepoHandler) => {
+interface IOptions {
+  warnMissingDirectory?: boolean;
+}
+
+export default async (context: IMigrationContext, param1: (RepoHandler | IOptions), param2?: RepoHandler) => {
   const {
     migration: {
       repos: migrationRepos,
@@ -15,7 +19,22 @@ export default async (context: IMigrationContext, handler: RepoHandler) => {
     adapter,
   } = context;
 
-  // if `selectedRepos` is specified, we should use that instead of the full repo list
+  let handler: RepoHandler;
+  let options: IOptions;
+  if (typeof param1 === 'function') {
+    // No options were provided
+    options = {};
+    handler = param1;
+  } else {
+    // We got options!
+    options = param1;
+    handler = param2 as RepoHandler;
+  }
+
+  // We want to show these warnings by default and allow opt-out of them
+  const { warnMissingDirectory = true } = options;
+
+  // If `selectedRepos` is specified, we should use that instead of the full repo list
   let repos;
   if (selectedRepos && selectedRepos.length) {
     repos = selectedRepos;
@@ -25,12 +44,13 @@ export default async (context: IMigrationContext, handler: RepoHandler) => {
 
   for (const repo of repos) {
     logger.info(chalk.bold(`\n[${adapter.stringifyRepo(repo)}]`));
+
     // Quick sanity check in case we're working from user-selected repos
     const repoDir = adapter.getRepoDir(repo);
-    if (!await existsAsync(repoDir)) {
-      // TODO disable this during the checkout phase
+    if (warnMissingDirectory && !await existsAsync(repoDir)) {
       logger.error(`Directory ${repoDir} does not exist`);
     }
+
     await handler(repo);
   }
 };
