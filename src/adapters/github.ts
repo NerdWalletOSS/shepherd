90c3a26cd519d4c40cae982969341657938ae159
Jugal Shah <jshah329@gmail.com>
2021-01-04 21:43:22 -0500

    feat: enable using search_type param with github adapter

--- src/adapters/github.ts
+++ src/adapters/github.ts
@@ -4,6 +4,7 @@ import chalk from 'chalk';
 import _ from 'lodash';
 import netrc from 'netrc';
 import path from 'path';
+import { reach } from '@hapi/hoek';
 
 import { IMigrationContext } from '../migration-context';
 import { paginate, paginateSearch } from '../util/octokit';
@@ -18,6 +19,7 @@ enum SafetyStatus {
 
 class GithubAdapter extends GitAdapter {
   private octokit: Octokit;
+  private static validSearchTypes = new Set(['code', 'repositories']);
 
   /**
    * Constructs a new GitHub adapter. The second parameter allows for
@@ -56,8 +58,12 @@ class GithubAdapter extends GitAdapter {
   }
 
   public async getCandidateRepos(onRetry: RetryMethod): Promise<IRepo[]> {
-    const { org, search_query } = this.migrationContext.migration.spec.adapter;
+    const { org, search_type, search_query } = this.migrationContext.migration.spec.adapter;
     let repoNames = [];
+
+    if (search_type && !GithubAdapter.validSearchTypes.has(search_type)) {
+      throw new Error(`Parameter "search_type" must be one of the following: ${Array.from(GithubAdapter.validSearchTypes).map(e => `'${e}'`).join(', ')}`);
+    }
 
     // list all of an orgs repos
     if (org) {
@@ -70,11 +76,26 @@ class GithubAdapter extends GitAdapter {
       const unarchivedRepos = repos.filter((r: any) => !r.archived);
       repoNames = unarchivedRepos.map((r: any) => r.full_name).sort();
     } else {
+      let searchMethod;
+      let fullNamePath = '';
+
+      switch (search_type) {
+        case 'repositories':
+          searchMethod = this.octokit.search.repos
+          fullNamePath = 'full_name'
+          break;
+        case 'code':
+        default:
+          searchMethod = this.octokit.search.code
+          fullNamePath = 'repository.full_name'
+      }
+
       // github code search query.  results are less reliable
-      const searchResults = await paginateSearch(this.octokit, this.octokit.search.code, onRetry)({
+      const searchResults = await paginateSearch(this.octokit, searchMethod, onRetry)({
         q: search_query,
       });
-      repoNames = searchResults.map((r: any) => r.repository.full_name).sort();
+
+      repoNames = searchResults.map((r: any) => reach(r, fullNamePath) ).sort();
     }
 
     return _.uniq(repoNames).map((r: string) => this.parseRepo(r));
