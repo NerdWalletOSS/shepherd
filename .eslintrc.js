7950c618320fceee8bdb0d1689d55f8105cbd7d3
Jugal Shah <jshah329@gmail.com>
2021-01-10 04:15:31 -0500

    chore: revert prior eslint change

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,6 @@ module.exports = {
     settings: {
         "import/resolver": {
           "node": {
-            "paths": ["src"],
             "extensions": [".js", ".jsx", ".ts", ".tsx"]
           }
         }
