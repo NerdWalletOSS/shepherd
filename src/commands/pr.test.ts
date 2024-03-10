75ba46950dcf3541dce2831e26381f9442fbaa6c
Aori Nevo <aorinevo@gmail.com>
2024-03-10 11:06:44 -0400

    chore(testing): add unit tests for pr command

---
+++ src/commands/pr.test.ts
@@ -0,0 +1,95 @@
+import pr from './pr';
+import { IMigrationContext } from '../migration-context';
+import mockAdapter from '../adapters/adapter.mock';
+import mockLogger from '../logger/logger.mock';
+import executeSteps from '../util/execute-steps';
+import { generatePrMessageWithFooter } from '../util/generate-pr-message';
+
+jest.mock('../util/execute-steps');
+jest.mock('../util/generate-pr-message');
+
+describe('pr commmand', () => {
+  let mockContext: IMigrationContext;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockContext = {
+      shepherd: {
+        workingDirectory: 'workingDirectory',
+      },
+      migration: {
+        migrationDirectory: 'migrationDirectory',
+        spec: {
+          id: 'id',
+          title: 'title',
+          adapter: {
+            type: 'adapter',
+          },
+          hooks: {},
+        },
+        workingDirectory: 'workingDirectory',
+        selectedRepos: [{ name: 'selectedRepos' }],
+        repos: [{ name: 'selectedRepos' }],
+        upstreamOwner: 'upstreamOwner',
+      },
+      adapter: mockAdapter,
+      logger: mockLogger,
+    };
+  });
+
+  it('should log error if no pr_message hook is specified', async () => {
+    await pr(mockContext);
+
+    expect(mockLogger.error).toHaveBeenCalledTimes(1);
+    expect(mockLogger.error).toHaveBeenCalledWith(
+      'No pr_message hook specified in the migration spec'
+    );
+  });
+
+  it('should call executeSteps for each repo', async () => {
+    mockContext.migration.spec.hooks.pr_message = ['pr_message'];
+    (executeSteps as jest.Mock).mockResolvedValueOnce({ succeeded: true, results: [] });
+    await pr(mockContext);
+
+    expect(executeSteps).toHaveBeenCalledTimes(1);
+    expect(executeSteps).toHaveBeenCalledWith(
+      mockContext,
+      { name: 'selectedRepos' },
+      'pr_message',
+      false
+    );
+  });
+
+  it('should create a pull request for each repo', async () => {
+    mockContext.migration.spec.hooks.pr_message = ['pr_message'];
+    (executeSteps as jest.Mock).mockResolvedValueOnce({ succeeded: true, results: [] });
+    (generatePrMessageWithFooter as jest.Mock).mockReturnValueOnce('footer message');
+    await pr(mockContext);
+
+    expect(mockAdapter.createPullRequest).toHaveBeenCalledTimes(1);
+    expect(mockAdapter.createPullRequest).toHaveBeenCalledWith(
+      { name: 'selectedRepos' },
+      'footer message',
+      'upstreamOwner'
+    );
+  });
+
+  it('handles create pull request error', async () => {
+    mockContext.migration.spec.hooks.pr_message = ['pr_message'];
+    (executeSteps as jest.Mock).mockResolvedValueOnce({ succeeded: true, results: [] });
+    (generatePrMessageWithFooter as jest.Mock).mockReturnValueOnce('footer message');
+    (mockAdapter.createPullRequest as jest.Mock).mockRejectedValueOnce('createPullRequest error');
+    await pr(mockContext);
+
+    expect(mockLogger.error).toHaveBeenCalledTimes(2);
+    expect(mockLogger.error).toHaveBeenCalledWith('createPullRequest error');
+  });
+
+  it('handles failed executeSteps', async () => {
+    mockContext.migration.spec.hooks.pr_message = ['pr_message'];
+    (executeSteps as jest.Mock).mockResolvedValueOnce({ succeeded: false, results: [] });
+    await pr(mockContext);
+
+    expect(generatePrMessageWithFooter).not.toHaveBeenCalled();
+  });
+});
